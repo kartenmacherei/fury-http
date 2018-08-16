ccc71381f6ce9087245524f281cd3a2b95f28f87
Mihailo Gajic <mihailo.gajic@kartenmacherei.de>
2018-08-16 14:09:44 +0200

    SHOP-633: adding CheckXdebugAvailableTrait

---
+++ tests/Unit/Helper/CheckXdebugAvailableTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+namespace Fury\UnitTests\Helper;
+
+trait CheckXdebugAvailableTrait
+{
+    public function checkXdebugGetHeadersIsAvailableOrSkipTest()
+    {
+        if (!function_exists('xdebug_get_headers')) {
+            $this->markTestSkipped('This test requires xdebug_get_headers() from the XDEBUG-Extension.');
+        }
+    }
+}
