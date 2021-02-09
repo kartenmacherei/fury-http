7ca349ba410b66171dcdc9344b8743e9ede997f0
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2021-02-09 15:46:22 +0100

    CDP-1190: Introduce XmlContent

---
+++ src/Application/Content/XmlContentType.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+namespace Kartenmacherei\HttpFramework\Application\Content;
+
+class XmlContentType extends ContentType
+{
+    /**
+     * @return string
+     */
+    public function asString(): string
+    {
+        return 'text/xml';
+    }
+}
