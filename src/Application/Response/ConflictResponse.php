944ee46b90818bd5dc25d4443cbe108b40dd08e0
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2021-02-11 14:41:35 +0100

    CDP-1190: Introduce ConflictResponse for ProductionOverview project

---
+++ src/Application/Response/ConflictResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+namespace Kartenmacherei\HttpFramework\Application\Response;
+
+use Kartenmacherei\HttpFramework\Http\Response\StatusCode;
+use Kartenmacherei\HttpFramework\Http\Response\StatusCode\ConflictStatusCode;
+
+class ConflictResponse extends ContentResponse
+{
+    protected function getStatusCode(): StatusCode
+    {
+        return new ConflictStatusCode();
+    }
+}
