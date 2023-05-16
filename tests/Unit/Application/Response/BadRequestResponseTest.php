51d0a90b78e79200343253901011d66ecd0bb022
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2023-05-16 13:50:35 +0200

    CDP-2277: Add BadRequestResponse for technical API for new packing machine

---
+++ tests/Unit/Application/Response/BadRequestResponseTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+namespace Kartenmacherei\HttpFramework\UnitTest\Application;
+
+use Kartenmacherei\HttpFramework\Application\Content\Content;
+use Kartenmacherei\HttpFramework\Application\Content\ContentType;
+use Kartenmacherei\HttpFramework\Application\Response\BadRequestResponse;
+use Kartenmacherei\HttpFramework\UnitTest\Helper\CheckXdebugAvailableTrait;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Kartenmacherei\HttpFramework\Application\Response\BadRequestResponse
+ */
+class BadRequestResponseTest extends TestCase
+{
+    use CheckXdebugAvailableTrait;
+
+    private const CONTENT_VALUE = 'foo';
+    private const CONTENT_TYPE_VALUE = 'text/html';
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSetsExpectedContentTypeHeader(): void
+    {
+        $this->checkXdebugGetHeadersIsAvailableOrSkipTest();
+
+        $this->expectOutputString(self::CONTENT_VALUE);
+        $content = $this->getContentMock();
+
+        $response = new BadRequestResponse($content);
+        $response->send();
+
+        $this->assertSame(
+            ['Content-Type: text/html; charset=UTF-8'], xdebug_get_headers()
+        );
+    }
+
+    /**
+     * @return MockObject|Content
+     */
+    private function getContentMock()
+    {
+        $contentMock = $this->createMock(Content::class);
+        $contentTypeMock = $this->createMock(ContentType::class);
+
+        $contentMock->expects($this->once())
+            ->method('asString')
+            ->willReturn(self::CONTENT_VALUE);
+
+        $contentMock->expects($this->once())
+            ->method('getContentType')
+            ->willReturn($contentTypeMock);
+
+        $contentTypeMock->expects($this->once())
+            ->method('asString')
+            ->willReturn(self::CONTENT_TYPE_VALUE);
+
+        return $contentMock;
+    }
+}
